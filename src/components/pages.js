335d268831172846ed2a538fbede687ebfd81017
Marco Escaleira <marcoescaleira2000@gmail.com>
2019-07-27 23:13:14 +0100

    Header page name fix

--- src/components/pages.js
+++ src/components/pages.js
@@ -1,5 +1,5 @@
 export default [
-  { name: 'Home', path: '/' },
+  { name: 'Início', path: '/' },
   { name: 'Serviços', path: '/services' },
   { name: 'Onde Estamos', path: '/whereWeAre' },
   { name: 'Contactos', path: '/contacts' },
